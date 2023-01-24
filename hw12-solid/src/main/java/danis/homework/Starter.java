a36019bcc8a803af11d627b09d32047c5485964d
Денис <anisimovets@mail.ru>
2023-01-24 13:16:29 +0300

    hw06 Refactoring 2

--- hw12-solid/src/main/java/danis/homework/Starter.java
+++ hw12-solid/src/main/java/danis/homework/Starter.java
@@ -4,13 +4,13 @@ public class Starter {
 
     public static void main(String[] args) {
         ATM atm = new ATM(new Storage());
-        System.out.println(StorageUtil.getBalance(atm.getStorage()));
+        StorageUtil.showBanknotsInStorage(atm.getStorage());
         atm.addMoney(Banknote.Nom_10, 5);
         atm.addMoney(Banknote.Nom_100, 5);
         atm.addMoney(Banknote.Nom_50, 10);
-        System.out.println(StorageUtil.getBalance(atm.getStorage()));
+        StorageUtil.showBanknotsInStorage(atm.getStorage());
         atm.take(20);
-        System.out.println(StorageUtil.getBalance(atm.getStorage()));
+        StorageUtil.showBanknotsInStorage(atm.getStorage());
 
     }
 }
