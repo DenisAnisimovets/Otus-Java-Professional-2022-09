224573d22da61ff9f43308f024df6929047a7fd8
Денис <anisimovets@mail.ru>
2023-01-27 15:53:51 +0300

    hw09 init

---
+++ hw18-jdbc/homework/src/main/java/ru/otus/jdbc/mapper/EntityClassMetaDataImpl.java
@@ -0,0 +1,45 @@
+package ru.otus.jdbc.mapper;
+
+import ru.otus.crm.model.Id;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
+
+public class EntityClassMetaDataImpl<T> implements EntityClassMetaData{
+
+    private final Class<T> entityClass;
+    public EntityClassMetaDataImpl(Class<T> entityClass) {
+        this.entityClass = entityClass;
+    }
+
+    @Override
+    public String getName() {
+        return entityClass.getSimpleName();
+    }
+
+    @Override
+    public Constructor getConstructor() {
+        try {
+            return entityClass.getConstructor();
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    @Override
+    public Field getIdField() {
+        return Arrays.stream(entityClass.getDeclaredFields()).filter(it -> it.isAnnotationPresent(Id.class)).findFirst().get();
+    }
+
+    @Override
+    public List<Field> getAllFields() {
+        return Arrays.stream(entityClass.getDeclaredFields()).toList();
+    }
+
+    @Override
+    public List<Field> getFieldsWithoutId() {
+        return Arrays.stream(entityClass.getDeclaredFields()).filter(it -> !it.isAnnotationPresent(Id.class)).toList();
+    }
+}
