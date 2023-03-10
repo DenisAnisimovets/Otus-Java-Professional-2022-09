105b16939cefd8200d9f6985dfa0c96196c07769
Денис <anisimovets@mail.ru>
2023-03-10 18:02:09 +0300

    hw17

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -6,5 +6,9 @@ public interface Versions {
     String stomp = "2.3.4";
     String bootstrap = "5.2.3";
 
+    String errorProneAnnotations = "2.11.0";
+    String tomcatAnnotationsApi = "6.0.53";
+    String grpc = "1.44.1";
+    String wiremock = "3.0.0-beta-2";
     String r2dbcPostgresql = "0.8.13.RELEASE";
 }
